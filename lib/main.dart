fa83792ae952787d61ee6e2c8df0515c15b16efc
Thalyson <thalyon.silva@ifce.edu.br>
2022-06-23 18:03:08 -0300

    [DEV] Add DB provider into app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_timelapse/app.dart';
+import 'package:provider/provider.dart';
+
+import 'db/db.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DB(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
